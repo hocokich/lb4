395b83b6a4ad1e3b4a1f48c83a9e68e5ab8de5d4
Александр <bondarenco65@gmail.com>
2024-10-01 14:49:07 +0700

    ...

--- lb4.button/Form1.Designer.cs
+++ lb4.button/Form1.Designer.cs
@@ -72,7 +72,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 694);
+            this.ClientSize = new System.Drawing.Size(529, 694);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.hoverButtonB1);
             this.Controls.Add(this.hoverButton1);
--- lb4.button/Form1.cs
+++ lb4.button/Form1.cs
@@ -77,5 +77,10 @@ namespace lb4.button
                 return;
             }
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
